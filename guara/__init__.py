75ae65f0811bf1f3db36b7ce00307e25f53eeff7
Felipe Vianna Rodrigues <felipevr98@poli.ufrj.br>
2022-02-14 03:54:16 -0300

    Adicionar arquivo __init__ na pasta
    Assim o python interpreta a pasta como uma biblioteca

---
+++ guara/__init__.py
@@ -0,0 +1,10 @@
+"""
+[REF] https://docs.python.org/3/tutorial/modules.html#packages
+
+Este arquivo nao deve ser apagado
+
+Ele serve para que o python interprete a pasta atual como
+um modulo, isto eh, uma biblioteca python. Entao podemos
+importar os arquivos que estao aqui dentro como pacotes da biblioteca
+usando o comando 'import'.
+"""
