ed40dbb74780940217cb1f1a2859751f955d630c
Felipe Vianna Rodrigues <felipevr98@poli.ufrj.br>
2022-02-16 00:57:01 -0300

    Retirar implementacoes antigas de bfs e dfs

--- guara/paths.py
+++ guara/paths.py
@@ -3,183 +3,6 @@ import numpy as np
 from guara.graph import Graph
 from guara.utils import weighted_heap
 
-def breadth_search_mtx(graph, seed=0):
-    # visited[v]: flag booleana indicando se o vertice v ja foi investigado
-    #
-    visited = np.zeros(len(graph), dtype=np.int8)
-    visited[seed] = True # iniciamos a descoberta dos vertices pela semente
-
-    # distance[v]: representa a distancia, em arestas, de v ate a raiz, eh equivalente ao nivel do
-    #    vertice na arvore gerada pela busca
-    #
-    distance = np.full(len(graph), fill_value=(-1), dtype=np.int16) # array de tamanho n preenchido com -1
-
-    # parent[v]: o indice do vertice pai de v na arvore gerada pela busca
-    #
-    parent = np.full(len(graph), fill_value=(-1), dtype=np.int32) # array de tamanho n preenchido com -1
-
-    # [NOTE] Se um vertice terminar com distance[v] == -1 e/ou parent[v] == -1, significa que ele nao
-    #    esta conectado a raiz
-
-    distance[seed] = 0 # definimos o nivel da raiz como 0
-    parent[seed] = seed # colocamos a raiz como pai de si mesma
-
-    # queue: fila com os indices dos vertices que precisam ser investigados
-    #   |_ queue[0]: o vertice a ser investigado no momento
-    #
-    queue = [seed]
-
-    while (len(queue) != 0):
-        current = queue[0] # pegamos o indice do vertice sendo investigado
-        queue.pop(0) # retiramos ele da fila
-
-        for vert in range(len(graph)): # iremos percorrer todos os vertices para ver se sao vizinhos do atual
-            if graph[current][vert]: # existe uma aresta (current,vert), isto eh, eles sao vizinhos
-                if vert != current: # para evitar loops, ignoramos arestas do tipo (i,i)
-                    if not visited[vert]: # vert ainda nao foi investigado
-                        queue.append(vert) # adicionamos vert a fila para ser investigado
-                        visited[vert] = True # indicamos que vert ja foi descoberto
-                        
-                        distance[vert] = distance[current] + 1 # declaramos vert como estando 1 nivel acima de current
-                        parent[vert] = current # declaramos current como o vertice pai de vert na arvore
-
-    return (distance, parent)
-
-# -----------------------------------------------
-
-
-def breadth_search_lst(graph, seed=0):
-    # visited[v]: flag booleana indicando se o vertice v ja foi investigado
-    #
-    visited = np.zeros(len(graph), dtype=np.int8)
-    visited[seed] = True # iniciamos a descoberta dos vertices pela semente
-
-    # distance[v]: representa a distancia, em arestas, de v ate a raiz, eh equivalente ao nivel do
-    #    vertice na arvore gerada pela busca
-    #
-    distance = np.full(len(graph), fill_value=(-1), dtype=np.int16) # array de tamanho n preenchido com -1
-
-    # parent[v]: o indice do vertice pai de v na arvore gerada pela busca
-    #
-    parent = np.full(len(graph), fill_value=(-1), dtype=np.int32) # array de tamanho n preenchido com -1
-
-    distance[seed] = 0 # definimos o nivel da raiz como 0
-    parent[seed] = seed # colocamos a raiz como pai de si mesma
-
-    # queue: fila com os indices dos vertices que precisam ser investigados
-    #   |_ queue[0]: o vertice a ser investigado no momento
-    #
-    queue = [seed]
-
-    while (len(queue) != 0):
-        current = queue[0] # pegamos o indice do vertice sendo investigado
-        queue.pop(0) # retiramos ele da fila
-
-        for neighbor in graph[current]: # verificamos todos os vizinhos do vertice sendo investigado
-            if neighbor != current: # para evitar loops, ignoramos arestas do tipo (i,i)
-                if not visited[neighbor]: # o vizinho ainda nao foi investigado
-                    queue.append(neighbor) # adicionamos o vizinho a lista para ser investigado
-                    visited[neighbor] = True # indicamos que o vizinho ja foi descoberto
-        
-                    distance[neighbor] = distance[current] + 1 # declaramos neighbor como estando 1 nivel acima de cur
-                    parent[neighbor] = current # declaramos cur como o vertice pai de neighbor na arvore
-
-    return (distance, parent)
-
-# -----------------------------------------------
-
-
-def depth_search_mtx(graph, seed=0):
-    # visited[v]: flag booleana indicando se o vertice v ja foi investigado
-    #
-    visited = np.zeros(graph.n, dtype=np.uint8) # flags indicando se o vertice ja foi visitado
-
-    # distance[v]: representa a distancia, em arestas, de v ate a raiz, eh equivalente ao nivel do
-    #    vertice na arvore gerada pela busca
-    #
-    distance = np.full(len(graph), fill_value=(-1), dtype=np.int16) # array de tamanho n preenchido com -1
-
-    # parent[v]: o indice do vertice pai de v na arvore gerada pela busca
-    #
-    parent = np.full(len(graph), fill_value=(-1), dtype=np.int32) # array de tamanho n preenchido com -1
-
-    # [NOTE] Se um vertice terminar com distance[v] == -1 e/ou parent[v] == -1, significa que ele nao
-    #    esta conectado a raiz
-
-    distance[seed] = 0 # definimos o nivel da raiz como 0
-    parent[seed] = seed # colocamos a raiz como pai de si mesma
-
-    # stack: pilha com os indices dos vertices que precisam ser investigados
-    #   |_ stack[-1]: o vertice a ser investigado no momento
-    #
-    stack = [seed]
-
-    while (len(stack) != 0):
-        current = stack[-1] # pegamos o indice do vertice sendo investigado
-        stack.pop(-1) # retiramos ele da pilha
-        
-        if not visited[current]:
-            visited[current] = True
-
-            for vert in range(len(graph))[::-1]: # pegamos todos os elementos da lista usando step=-1
-                # seguindo o padrao da aula 5 (slide 18) percorremos os vertices em ordem descrescente
-                # desse modo os vizinhos de menor indice ficam sempre no topo da pilha, isto eh, sao analisados primeiro
-                if graph[current][vert]: # se os dois forem vizinhos
-                    if not visited[vert]: # se o vizinho ja foi visitado, nao ha pq adiciona-lo na pilha
-                        stack.append(vert) # adicionamos o vizinho na pilha
-
-                        distance[vert] = distance[current] + 1 # o nivel do noh vert eh um nivel acima do atual
-                        parent[vert] = current # o pai do noh vert eh o no sendo analisado
-
-    return (distance, parent)
-
-# -----------------------------------------------
-
-
-def depth_search_lst(graph, seed=0):
-    # visited[v]: flag booleana indicando se o vertice v ja foi investigado
-    #
-    visited = np.zeros(graph.n, dtype=np.uint8) # flags indicando se o vertice ja foi visitado
-
-    # distance[v]: representa a distancia, em arestas, de v ate a raiz, eh equivalente ao nivel do
-    #    vertice na arvore gerada pela busca
-    #
-    distance = np.full(len(graph), fill_value=(-1), dtype=np.int16) # array de tamanho n preenchido com -1
-
-    # parent[v]: o indice do vertice pai de v na arvore gerada pela busca
-    #
-    parent = np.full(len(graph), fill_value=(-1), dtype=np.int32) # array de tamanho n preenchido com -1
-
-    # [NOTE] Se um vertice terminar com distance[v] == -1 e/ou parent[v] == -1, significa que ele nao
-    #    esta conectado a raiz
-
-    distance[seed] = 0 # definimos o nivel da raiz como 0
-    parent[seed] = seed # colocamos a raiz como pai de si mesma
-
-    # stack: pilha com os indices dos vertices que precisam ser investigados
-    #   |_ stack[-1]: o vertice a ser investigado no momento
-    #
-    stack = [seed]
-
-    while (len(stack) != 0):
-        current = stack[-1] # pegamos o indice do vertice sendo investigado
-        stack.pop(-1) # retiramos ele da pilha
-        
-        if not visited[current]:
-            visited[current] = True
-
-            # [REF] https://stackoverflow.com/questions/6771428/most-efficient-way-to-reverse-a-numpy-array
-            for neighbor in graph[current][::-1]: # pegamos todos os elementos da lista usando step=-1
-                # seguindo o padrao da aula 5 (slide 18) percorremos os vertices em ordem descrescente
-                # desse modo os vizinhos de menor indice ficam sempre no topo da pilha, isto eh, sao analisados primeiro
-                if not visited[neighbor]: # se o vizinho ja foi visitado, nao ha pq adiciona-lo na pilha
-                    stack.append(neighbor) # adicionamos o vizinho na pilha
-
-                    distance[neighbor] = distance[current] + 1 # o nivel do noh neighbor eh um nivel acima do atual
-                    parent[neighbor] = current # o pai do noh neighbor eh o no sendo analisado
-
-    return (distance, parent)
-
 # -----------------------------------------------
 
 
@@ -453,12 +276,17 @@ def dijkstra(graph, s):
 
 # def floyd_warshall(graph):
 
+#     # dist[i,j]: contem o custo total do menor caminho que se conhece que vai de i para j
 #     dist = np.full( shape=(graph.n, graph.n), fill_value=np.inf)
 
-#     for v in graph:
-#         dist[v][ graph.neighbors(v) ] = graph.weights(v)
-#         dist[v,v] = 0
+#     neg_cycle = False # flag indicando se foi encontrado um ciclo negativo no grafo
 
+#     # inicializamos dist com o custo para ir de um vertice a outro sem vertices intermediarios
+#     for v in graph:
+#         dist[v][ graph.neighbors(v) ] = graph.weights(v) # selecionamos, na linha v, os elementos correpospondentes aos vizinhos de v
+#         dist[v,v] = 0 # o custo de ir de v para v eh nulo
+
+#     # parent[i,j]: contem o vertice anterior a j no menor caminho que se conhece de i para j (isto eh, o pai de j)
 #     parent = np.full( shape=(graph.n, graph.n), fill_value=-1)
 
 #     for v in graph:
@@ -466,25 +294,25 @@ def dijkstra(graph, s):
 #         parent[v,v] = v
 
 #     for k in range(1, graph.n):
-#         for i in range(1, graph.n):
-#             for j in range(1, graph.n):
-#                 print(f'i:{i}, j:{j}, k:{k}')
-#                 print(dist)
-#                 print(parent)
-#                 if ((dist[i,k]+dist[k,j]) < dist[i,j]):
+#         # comecamos o k em 1, pois k = 0 eh a propria inicializacao da matriz
+#         # print(f'k:{k-1}')
+#         # print(dist)
+#         # print(parent)
+#         for i in range(graph.n):
+#             for j in range(graph.n):
+#                 if (dist[i,k] + dist[k,j]) < dist[i,j]:
 
-#                     if (i != j) or (k > 1):
-#                     # verificamos para nao tratar uma aresta negativa como um ciclo negativo, ja que o grafo eh nao direcionado
-#                         dist[i,j] = (dist[i,k] + dist[k,j])
-#                         parent[i,j] = parent[k,j]
+#                     dist[i,j] = (dist[i,k] + dist[k,j])
+#                     parent[i,j] = parent[i,k]
 
-#                     if (dist[i,i] < 0):
-#                         print('---- ',dist[i,i])
-#                         print(dist)
-#                         print(parent)
-#                         raise Exception(f'Encontrado ciclo negativo no grafo {graph.name}. Nao foi possivel calcular distancias.')
+#                     if (i == j) and (dist[i,i] < 0):
+#                         neg_cycle = True
+#                         # print(f'i:{i}, j:{j}, k:{k}')
+#                         # print(dist)
+#                         # print(parent)
+#                         # raise Exception(f'Encontrado ciclo negativo no grafo {graph.name}. Nao foi possivel calcular distancias.')
 
-#     return dist, parent
+#     return dist, parent, neg_cycle
 
 # -----------------------------------------------
 
