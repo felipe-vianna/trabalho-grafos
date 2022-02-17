3472990ce1d3e3d461c22f8a1d73210d03e777bf
Felipe Vianna Rodrigues <felipevr98@poli.ufrj.br>
2022-02-16 21:06:30 -0300

    Atualizar implementacao do heap
    Na implementacao anterior, a busca de um elemento tinha complexidade
    O(n). Agora ha a adicao de um dicionario que permite a busca em acesso direto

--- guara/utils.py
+++ guara/utils.py
@@ -6,145 +6,171 @@ class weighted_heap():
     def __init__(self):
         self.queue = [] # a fila de prioridade
         # [TODO] implementar tabela de acesso a fila para otimizar a atualizacao de um elemento
+        self.lookup = {} # tabela de acesso de elementos do heap por meio da chave
 
     def __len__(self):
         return len(self.queue)
 
+    def __getitem__(self, key):
+        return self.queue[ self.lookup[key] ]
+
     # -------------------------------------------------------------------
 
-    def insert(self, key, weight):
+    def push(self, key, weight):
         """
         Insere um elemento dado por uma chave (identificador) e um peso. Reordena os elementos da
         fila com base nos seus pesos.
         Complexidade: O(log(n))
         """
 
+        self.lookup[key] = len(self.queue)
         self.queue.append((key, weight)) # adicionamos o elemento no final do heap
 
-        # print(self.queue)
+        print(self.queue)
 
-        self.climb(index=(len(self.queue) - 1)) # subimos o elemento para o local correto no heap
+        self.climb(key) # subimos o elemento para o local correto no heap
 
     # -------------------------------------------------------------------
 
-    def remove(self):
+    def pop(self):
         """
         Tira o primeiro elemento da fila e a reordena. Retorna o primeiro elemento.
         Complexidade: O(log(n))
         """
-
         if len(self.queue) == 0:
             return None
 
-        root = self.queue[0] # o primeiro elemento do heap
 
         if len(self.queue) > 1:
-            self.queue[0] = self.queue.pop(-1) # retiramos o ultimo elemento e colocamos no topo do heap
-        else:
-            self.queue.pop(-1) # retiramos o ultimo elemento do heap
+            self.switch(self.queue[0], self.queue[-1])
 
-        # print(self.queue)
+        # lembrando que o ultimo e o primeiro elemento foram trocados
+        last = self.queue[0]
+        first = self.queue.pop(-1)
+        del self.lookup[ first[0] ]
 
-        self.fall(index=0) # descemos o elemento que foi colocado no topo para o local correto
+        print(self.queue)
 
-        return root
+        self.fall(key=last[0]) # descemos o elemento que foi colocado no topo para o local correto
+
+        return first
 
     # -------------------------------------------------------------------
 
     def update(self, key, new_weight):
         """
         Atualiza o peso de um elemento, identificado pela chave, e reordena o heap.
-        Complexidade: O(n) (pois precisa buscar a chave em todos os elementos, pode mudar futuramente)
+        Complexidade: O(log(n))
         """
+        if len(self.queue) == 0:
+            return None
 
-        queue_index = 0
-        for idx, elem in enumerate(self.queue):
-            if elem[0] == key:
-                queue_index = idx
-                self.queue[idx] = (key, new_weight)
-                break
-
-        # print(self.queue)
+        idx = self.lookup[key]
+        self.queue[idx] = (key, new_weight)
 
         # subimos (ou descemos, o que for possivel) o elemento modificado ate o heap estar ordenado
-        self.fall(queue_index)
-        self.climb(queue_index)
+        self.fall(key)
+        self.climb(key)
     
     # -------------------------------------------------------------------
 
-    def climb(self, index):
+    def climb(self, key):
         """
         Recebe um indice de elemento na fila e "sobe" esse elemento (troca ele com seus pais)
         ate que a ordem correta do heap seja alcancada.
         """
-        parent_index = (index - 1)//2 # o indice na fila do pai do elemento que esta subindo
+        if len(self.queue) == 0:
+            return None
+
+        elem = self[key]
+        parent = self.parent(key) # o pai do elemento que esta subindo
+
+        while (parent is not None) and (parent[1] > elem[1]):
+            # se o peso do pai for maior que do elemento atual, trocamos a posicao deles na fila
+            self.switch(elem, parent)
+
+            print(self.queue)
+
+            parent = self.parent(key)
+
+        return None
+
+    # -------------------------------------------------------------------
+
+    def fall(self, key):
+        """
+        Recebe um indice na fila de um elemento e faz esse elemento "cair" (troca-o de lugar com o
+        menor dos seus filhos) ate que o heap esteja ordenado.
+        """
+        if len(self.queue) == 0:
+            return None
+
+        elem = self[key]
+        child = self.smaller_child(key) # o menor filho do elemento que esta descendo
+
+        while (child is not None) and (child[1] < elem[1]):
+            # se o peso do filho for menor que do elemento atual, trocamos a posicao deles na fila
+            self.switch(elem, child)
+
+            print(self.queue)
+
+            child = self.smaller_child(key)
+
+        return None
+
+    # -------------------------------------------------------------------
+
+    def parent(self, key):
+        """
+        Recebe a chave de um elemento da fila e retorna o pai dele.
+        """
+        # elem_index = self.lookup[key]
+        parent_index = (self.lookup[key] - 1)//2 # o indice do pai do elemento
 
         if parent_index < 0:
             # se o indice do pai na fila for negativo, eh pq ele nao existe e nao ha como subir
             return None
 
-        while self.queue[parent_index][1] > self.queue[index][1]:
-            # se o peso do pai for maior que o elemento atual, trocamos a posicao deles na fila
-            temp = self.queue[parent_index]
-            self.queue[parent_index] = self.queue[index]
-            self.queue[index] = temp
-
-            # print(self.queue)
-
-            # atualizamos o indice do elemento que esta subindo e do seu pai
-            index = parent_index
-            parent_index = (index - 1)//2
-
-            if parent_index < 0:
-                # se o indice do pai na fila for negativo, nao ha mais como subir
-                return None
+        return self.queue[parent_index]
 
     # -------------------------------------------------------------------
 
-    def fall(self, index):
+    def smaller_child(self, key):
         """
-        Recebe um indice na fila de um elemento e faz esse elemento "cair" (troca-o de lugar com o
-        menor dos seus filhos) ate que o heap esteja ordenado.
+        Recebe a chave de um elemento da fila e retorna o filho dele que possuir o menor peso.
         """
-
-        child_index = self.get_smaller_child(index)
-
-        if child_index is None:
-            return None
-
-        # -----------------------------------
-        while (self.queue[index][1] > self.queue[child_index][1]):
-            # trocamos a posicao dos elementos na fila
-            temp = self.queue[index]
-            self.queue[index] = self.queue[child_index]
-            self.queue[child_index] = temp
-
-            # print(self.queue)
-
-            index = child_index
-
-            child_index = self.get_smaller_child(index)
-
-            if child_index is None:
-                return None
-
-    # -------------------------------------------------------------------
-
-    def get_smaller_child(self, index):
-        """
-        Recebe o indice de um elemento na fila e retorna o indice do filho que possuir o menor peso.
-        """
-        child_index = (2 * index) + 1 # o indice do primeiro filho (caso exista)
+        # elem_index = self.lookup[key]
+        child_index = (2 * self.lookup[key]) + 1 # o indice do primeiro filho (caso exista)
 
         if child_index >= len(self.queue): # verificamos se o filho existe
-            return None
+            return None # o filho nao existe
 
-        child_index_2 = child_index + 1 # o indice do segundo filho (caso exista)
 
-        if child_index_2 < len(self.queue): # verificamos se o segundo filho existe
+        if child_index < (len(self.queue) - 1): # verificamos se o segundo filho existe
+
+            child_index_2 = child_index + 1 # o indice do segundo filho (caso exista)
 
             # pegaremos o filho com menor peso
             if (self.queue[child_index_2][1] < self.queue[child_index][1]):
                 child_index = child_index_2
 
-        return child_index
+        return self.queue[child_index]
+
+    # -------------------------------------------------------------------
+    
+    def switch(self, elem1, elem2):
+        """
+        Recebe dois elementos (tupla) do heap e troca eles de lugar, atualizando a tabela 
+        de acesso de acordo.
+        """
+
+        idx1 = self.lookup[ elem1[0] ]
+        idx2 = self.lookup[ elem2[0] ]
+
+        # trocamos a posicao dos elementos na fila
+        self.queue[idx1] = elem2
+        self.queue[idx2] = elem1
+
+        # trocamos os indices na tabela de acesso
+        self.lookup[ elem1[0] ] = idx2
+        self.lookup[ elem2[0] ] = idx1
